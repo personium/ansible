77890bf09734d4ce6f461aa4f87517606e8bd945
KourokiTakuto <fj3768cq@aa.jp.fujitsu.com>
2018-05-31 18:15:41 +0900

    It corresponds to Issue below. #21 `git clone core repo` is not idempotent due to LF normalization #22 `Setting default zone` fails if the firewalld was already started #23 Missing cacert.crt #26 An error occurs when Tomcat starts up before Elasticsearch close #21, close #22, close #23, close #26

---
+++ 3-server_unit/resource/es/personium/check_cluster_health.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Shell script for checking Elasticsearch's cluster health
+#
+
+ES_IP=$1
+CLUSTER_HEALTH_STATUS=red
+
+while [ $CLUSTER_HEALTH_STATUS = "red" ]
+do
+    CLUSTER_HEALTH_STATUS=`curl -X GET 'http://${ES_IP}:9200/_cluster/health' | cut -d',' -f2 | cut -d':' -f2 | tr -d '"'`
+    
+    if [ $CLUSTER_HEALTH_STATUS != "green" && $CLUSTER_HEALTH_STATUS != "yellow" ]; then
+        CLUSTER_HEALTH_STATUS=red
+        sleep 30
+    fi
+
+done

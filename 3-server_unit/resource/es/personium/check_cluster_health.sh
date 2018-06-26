418e6d7d1b04b7b45a146463fcecd6cc06b6f9af
mokudai.naoya <mokudai.naoya@jp.fujitsu.com>
2018-06-26 10:20:27 +0900

    close #34

--- 3-server_unit/resource/es/personium/check_cluster_health.sh
+++ 3-server_unit/resource/es/personium/check_cluster_health.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Shell script for checking Elasticsearch's cluster health
+# Copyright 2016 - 2018 FUJITSU LIMITED
 #
 
 ES_IP=$1
@@ -9,7 +10,7 @@ CLUSTER_HEALTH_STATUS=red
 while [ $CLUSTER_HEALTH_STATUS = "red" ]
 do
     CLUSTER_HEALTH_STATUS=`curl -X GET 'http://${ES_IP}:9200/_cluster/health' | cut -d',' -f2 | cut -d':' -f2 | tr -d '"'`
-    
+
     if [ $CLUSTER_HEALTH_STATUS != "green" && $CLUSTER_HEALTH_STATUS != "yellow" ]; then
         CLUSTER_HEALTH_STATUS=red
         sleep 30
